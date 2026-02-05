b92d1c597adda2f457db77ea6c51069422ac9699
Larry La <larryquocla@gmail.com>
2026-02-04 16:08:09 -0800

    feat: dll implementation c

--- Homework_1/Chapter_1/Question_3.c
+++ Homework_1/Chapter_1/Question_3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Node {
     char* data;
@@ -8,40 +9,68 @@ struct Node {
 };
 
 void insert(struct Node** head, char* data) {
-    // create new node
-    struct Node* newNode;
-    
-    // allocate mem
-    newNode = malloc(sizeof(struct Node));
+    struct Node* newNode = malloc(sizeof(struct Node));
+    if (!newNode) return;
 
-    // allocate mem for data
     newNode->data = malloc(strlen(data) + 1);
+    if (!newNode->data) return;
 
-    // copy the data
     strcpy(newNode->data, data);
 
-    // set new node prev = none
     newNode->previous = NULL;
     newNode->next = *head;
 
-    // if head 
     if (*head != NULL) {
         (*head)->previous = newNode;
+    }
 
-    };
-
-    *head = newNode;
+    *head = newNode;   
 }
 
-void find(struct Node* head, char* target) {
-
+struct Node* find(struct Node* head, char* target) {
+    while (head != NULL) {
+        if (strcmp(head->data, target) == 0) {
+            return head;
+        }
+        head = head->next;
+    }
+    return NULL;
 }
 
 void delete(struct Node** head, char* target) {
+    struct Node* target_node = find(*head, target);
 
+    if (target_node == NULL) return;
+
+    if (target_node->previous != NULL) {
+        target_node->previous->next = target_node->next;
+    } else {
+        *head = target_node->next;
+    }
+
+    if (target_node->next != NULL) {
+        target_node->next->previous = target_node->previous;
+    }
+
+    free(target_node->data);
+    free(target_node);
 }
 
 int main() {
-    struct Node* head = NULL;
+    struct Node* head = NULL;     
+
+    insert(&head, "5");
+    insert(&head, "6");
+    insert(&head, "7");
+
+    delete(&head, "6");
+
+    struct Node* curr = head;
+    while (curr != NULL) {
+        printf("%s ", curr->data);
+        curr = curr->next;
+    }
+    printf("\n");
+
     return 0;
 }
